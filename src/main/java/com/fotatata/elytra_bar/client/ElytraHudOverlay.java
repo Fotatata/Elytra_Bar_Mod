9383565421f081d854a5b588796db71a5bafd014
Fotatata <ranaranita16@gmail.com>
2024-02-14 20:38:51 -0500

    Updated to 1.20

--- src/main/java/com/fotatata/elytra_bar/client/ElytraHudOverlay.java
+++ src/main/java/com/fotatata/elytra_bar/client/ElytraHudOverlay.java
@@ -3,84 +3,79 @@ package com.fotatata.elytra_bar.client;
 import com.fotatata.elytra_bar.ElytraBar;
 import com.fotatata.elytra_bar.bar.DataHandler;
 import com.fotatata.elytra_bar.config.ClientConfig;
-import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.client.gui.GuiComponent;
-import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.client.gui.overlay.ForgeGui;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
 
-public class ElytraHudOverlay {
+public class ElytraHudOverlay implements IGuiOverlay {
     private static final ResourceLocation ELYTRA_ICONS = new ResourceLocation(ElytraBar.ModId,"textures/bar/icons.png");
 
-    public static final IGuiOverlay HUD_ELYTRA = (gui, poseStack, partialTick, width, height) -> {
-        int x = width/2;
-        int y;
-        int z;
+    @Override
+    public void render(ForgeGui gui, GuiGraphics graphics, float partialTick, int screenWidth, int screenHeight) {
+        int x = screenWidth/2;
+        int y = 0;
+        int v;
 
-        if (DataHandler.isWearingArmor() ^ DataHandler.hasAbsorption()) {
-            y = 0;
-        } else if (DataHandler.isWearingArmor() && DataHandler.hasAbsorption()) {
-            y = -10;
-        } else {
-            y = 10;
+        if (DataHandler.isWearingArmor()) {
+            y -= 10;
+        }
+        if (DataHandler.hasAbsorption()) {
+            y -= 10;
         }
 
         if (ClientConfig.ICON_TYPE.get()){
-            z = 9;
+            v = 9;
         }else{
-            z = 0;
+            v = 0;
         }
-
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderColor(1.0F,1.0F,1.0F,1.0F);
-        RenderSystem.setShaderTexture(0, ELYTRA_ICONS);
         if (DataHandler.IsWearingElytra()) {
             switch (ClientConfig.OVERLAY_TYPE.get()) {
                 case 0 -> {
                     for (int i = 0; i < 10; i++) {
-                        GuiComponent.blit(poseStack, x - 91 + (i * 8), y + height - 59, 0, z, 9, 9, 32, 32);
+                        graphics.blit(ELYTRA_ICONS, x - 91 + (i * 8), y + screenHeight - 49, 0, v, 9, 9,32,32);
                     }
                     for (int i = 0; i < DataHandler.getElytraDurability() / 2; i++) {
-                        GuiComponent.blit(poseStack, x - 91 + (i * 8), y + height - 59, 9, z, 9, 9, 32, 32);
+                        graphics.blit(ELYTRA_ICONS, x - 91 + (i * 8), y + screenHeight - 49, 9, v, 9, 9,32,32);
                     }
                     if ((DataHandler.getElytraDurability() % 2) == 1) {
-                        GuiComponent.blit(poseStack, x - 95 + (4 * DataHandler.getElytraDurability()), y + height - 59, 18, z, 9, 9, 32, 32);
+                        graphics.blit(ELYTRA_ICONS, x - 95 + (4 * DataHandler.getElytraDurability()), y + screenHeight - 49, 18, v, 9, 9,32,32);
                     }
                 }
                 case 1 -> {
                     for (int i = 0; i < 79; i++) {
-                        GuiComponent.blit(poseStack, x - 90 + i, y + height - 54, 1, 18, 1, 4, 32, 32);
+                        graphics.blit(ELYTRA_ICONS, x - 90 + i, y + screenHeight - 44, 1, 18, 1, 4,32,32);
                     }
                     for (int i = 0; i < 2; i++) {
-                        GuiComponent.blit(poseStack, x - 91 + (i*80), y + height - 54, 0, 18, 1, 4, 32, 32);
+                        graphics.blit(ELYTRA_ICONS, x - 91 + (i*80), y + screenHeight - 44, 0, 18, 1, 4,32,32);
                     }
                     for (int i = 1; i < DataHandler.getElytraDurability(); i++) {
-                        GuiComponent.blit(poseStack, x - 90 + i, y + height - 54, 3, 18, 1, 4, 32, 32);
+                        graphics.blit(ELYTRA_ICONS, x - 90 + i, y + screenHeight - 44, 3, 18, 1, 4,32,32);
                     }
                     if (DataHandler.getElytraDurability() >= 1) {
-                        GuiComponent.blit(poseStack, x - 90, y + height - 54, 2, 18, 1, 4, 32, 32);
+                        graphics.blit(ELYTRA_ICONS, x - 90, y + screenHeight - 44, 2, 18, 1, 4,32,32);
                     }
                 }
                 case 2 -> {
-                    GuiComponent.blit(poseStack, x - 91, y + height - 59, 0, z, 9 ,9 ,32, 32);
+                    graphics.blit(ELYTRA_ICONS, x - 91, y + screenHeight - 49, 0, v, 9 ,9,32,32);
                     if (DataHandler.getElytraDurability() >= 35)
-                        GuiComponent.blit(poseStack, x - 91, y + height - 59, 9, z, 9, 9, 32, 32);
+                        graphics.blit(ELYTRA_ICONS, x - 91, y + screenHeight - 49, 9, v, 9, 9,32,32);
                     if (DataHandler.getElytraDurability() >= 1 && DataHandler.getElytraDurability() <= 34)
-                        GuiComponent.blit(poseStack, x - 91, y + height - 59, 18, z, 9, 9, 32, 32);
+                        graphics.blit(ELYTRA_ICONS, x - 91, y + screenHeight - 49, 18, v, 9, 9,32,32);
                     for (int i = 0; i < 69; i++) {
-                        GuiComponent.blit(poseStack, x - 80 + i, y + height - 54, 1, 18, 1, 4, 32, 32);
+                        graphics.blit(ELYTRA_ICONS, x - 80 + i, y + screenHeight - 44, 1, 18, 1, 4,32,32);
                     }
                     for (int i = 0; i < 2; i++) {
-                        GuiComponent.blit(poseStack, x - 81 + (i*70), y + height - 54, 0, 18, 1, 4, 32, 32);
+                        graphics.blit(ELYTRA_ICONS, x - 81 + (i*70), y + screenHeight - 44, 0, 18, 1, 4,32,32);
                     }
                     for (int i = 1; i < DataHandler.getElytraDurability(); i++) {
-                        GuiComponent.blit(poseStack, x - 80 + i, y + height - 54, 3, 18, 1, 4, 32, 32);
+                        graphics.blit(ELYTRA_ICONS, x - 80 + i, y + screenHeight - 44, 3, 18, 1, 4,32,32);
                     }
                     if (DataHandler.getElytraDurability() >= 1) {
-                        GuiComponent.blit(poseStack, x - 80, y + height - 54, 2, 18, 1, 4, 32, 32);
+                        graphics.blit(ELYTRA_ICONS, x - 80, y + screenHeight - 44, 2, 18, 1, 4,32,32);
                     }
                 }
             }
         }
-    };
+    }
 }
--- src/main/java/com/fotatata/elytra_bar/event/ClientEvents.java
+++ src/main/java/com/fotatata/elytra_bar/event/ClientEvents.java
@@ -4,16 +4,17 @@ import com.fotatata.elytra_bar.ElytraBar;
 import com.fotatata.elytra_bar.client.ElytraHudOverlay;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
+import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 public class ClientEvents {
     @Mod.EventBusSubscriber(modid = ElytraBar.ModId, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
     public static  class ClientModBusEvents {
-
         @SubscribeEvent
-        public static void registerGuiOverlays(RegisterGuiOverlaysEvent event){
-            event.registerBelowAll("elytra_bar", ElytraHudOverlay.HUD_ELYTRA);
+        public static void registerElytraOverlay(RegisterGuiOverlaysEvent event) {
+            ElytraHudOverlay Hud = new ElytraHudOverlay();
+            event.registerAbove(VanillaGuiOverlay.HOTBAR.id(),"elytrahud", Hud);
         }
 
     }
